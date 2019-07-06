d5c6d4d98f3dbbe1db310be42d8247da4e3cf3aa
PatMan10 <PatMan10@protonmail.com>
2019-07-06 20:11:00 +0200

    added more

--- js/script.js
+++ js/script.js
@@ -1,13 +1,9 @@
 function openNavDrawer() {
   const navD = document.getElementById('nav-drawer')
-  const main = document.getElementById('main')
   navD.style.setProperty('width', '250px')
-  main.style.setProperty('margin-left', '250px')
 }
 
 function closeNavDrawer() {
   const navD = document.getElementById('nav-drawer')
-  const main = document.getElementById('main')
   navD.style.setProperty('width', '0')
-  main.style.setProperty('margin-left', '0')
 }
