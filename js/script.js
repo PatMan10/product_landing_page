d12a7c0aa210c15e736027651efaefa1f3a2a0eb
PatMan10 <PatMan10@protonmail.com>
2019-07-24 14:32:03 +0200

    added adjustYOffset function

--- js/script.js
+++ js/script.js
@@ -1,3 +1,17 @@
+function zol() {
+  console.log(window.innerWidth)
+}
+
+function adjustYOffset() {
+  setTimeout(() => {
+    const diff = window.innerWidth < 601 ? 100 : 130
+    const curY = window.pageYOffset
+    if (curY < diff) return
+    const newY = curY - diff
+    window.scrollTo(0, newY)
+  }, 5)
+}
+
 function openNavDrawer() {
   const navD = document.getElementById('nav-drawer')
   navD.style.setProperty('left', '0')
